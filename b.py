f4a42db4b878c67ad49620c15038039483e5eb19
Dragoon-galaxy <118508752+Dragoon-galaxy@users.noreply.github.com>
2024-05-16 18:47:10 +0530

    Delete b.py

--- b.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-from a import correlation_matrix
-
-# Plot the correlation matrix
-plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-plt.title('Correlation Matrix of Iris Dataset')
-plt.show()
